23913dcc63ce657f08f96d7ee8999426e619d533
cjun714 <cjun714@gmail.com>
2018-04-09 22:00:30 +0800

    modify menu to fix position at top

--- static/js/header.js
+++ static/js/header.js
@@ -6,10 +6,10 @@ var header = `
   </div>
   <nav id="nav-list">
     <ul>
-      <li><a href="/page/home.html">首页</a></li>
-      <li><a href="/page/solution.html">方案</a></li>
-      <li><a href="/page/case.html">案例</a></li>
-      <li><a href="/page/about.html">关于</a></li>
+      <li class='nav-home-url'><a href="/page/home.html">首页</a></li>
+      <li class='nav-solution-url'><a href="/page/solution.html">方案</a></li>
+      <li class='nav-case-url'><a href="/page/case.html">案例</a></li>
+      <li class='nav-about-url'><a href="/page/about.html">关于</a></li>
     </ul>
   </nav>
 </div>
